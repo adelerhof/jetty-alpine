04f094ebbd500d9ed6019b510725f90c5776608b
Jeroen de Wolf <wolfjde@hotmail.com>
2019-08-18 18:59:44 +0200

    Update MyResource.java

--- src/main/java/nl/vijfhart/MyResource.java
+++ src/main/java/nl/vijfhart/MyResource.java
@@ -4,6 +4,8 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.*;
+import javax.ws.rs.*;
 
 @Path("/java-webapp")
 public class MyResource extends extends Application {
