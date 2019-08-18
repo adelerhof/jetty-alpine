7284dec2eabed897c361ad6c20b23aea3e5a518b
Jeroen de Wolf <wolfjde@hotmail.com>
2019-08-18 22:34:14 +0200

    Update MyResource.java

--- src/main/java/nl/vijfhart/MyResource.java
+++ src/main/java/nl/vijfhart/MyResource.java
@@ -6,16 +6,10 @@ import javax.ws.rs.core.*;
 
 @Path("java-webapp")
 public class MyResource {
-
-    /**
-     * Method handling HTTP GET requests. The returned object will be sent
-     * to the client as "text/plain" media type.
-     *
-     * @return String that will be returned as a text/plain response.
-     */
+    
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String getIt() {
-        return "Hello from java-webapp";
+    public static String hello() {
+        return "Hello Java";
     }
 }
