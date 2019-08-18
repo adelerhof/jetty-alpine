05ebd11638ef23c3d8ff315de7d46cebeac54775
Jeroen de Wolf <wolfjde@hotmail.com>
2019-08-18 19:11:52 +0200

    Update MyResource.java

--- src/main/java/nl/vijfhart/MyResource.java
+++ src/main/java/nl/vijfhart/MyResource.java
@@ -1,10 +1,7 @@
 package nl.vijfhart;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.*;
+import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
 @ApplicationPath("/java-webapp")
